df609b92187c4eb57c1b824f9c301565c48ac229
Ignacio Roca de Togores <viniaiexport@gmail.com>
2026-07-14 10:46:26 +0200

    feat(checkout): registrar API-016

--- apps/backend/src/modules/checkout/checkout.module.ts
+++ apps/backend/src/modules/checkout/checkout.module.ts
@@ -1,4 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { CheckoutController } from "./checkout.controller.js";
+import { CheckoutRepository } from "./checkout.repository.js";
+import { CheckoutService } from "./checkout.service.js";
 
-@Module({})
+@Module({
+  controllers: [CheckoutController],
+  providers: [CheckoutRepository, CheckoutService],
+})
 export class CheckoutModule {}
