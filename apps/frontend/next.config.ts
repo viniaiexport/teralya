52b284ddcebb499827c7936e476b936495de17f4
Ignacio Roca de Togores <viniaiexport@gmail.com>
2026-07-14 11:51:07 +0200

    feat(frontend): añadir apps/frontend/next.config.ts

---
+++ apps/frontend/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+  typedRoutes: true,
+};
+
+export default nextConfig;
