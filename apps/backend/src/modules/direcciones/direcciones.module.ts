c272dba9aa1442638118b4e1cb9b05948308bafb
Ignacio Roca de Togores <viniaiexport@gmail.com>
2026-07-14 09:24:45 +0200

    feat(direcciones): update apps/backend/src/modules/direcciones/direcciones.module.ts

--- apps/backend/src/modules/direcciones/direcciones.module.ts
+++ apps/backend/src/modules/direcciones/direcciones.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../../common/database/database.module.js';
+import { SecurityModule } from '../../common/security/security.module.js';
+import { DireccionesController } from './direcciones.controller.js';
+import { DireccionesRepository } from './direcciones.repository.js';
+import { DireccionesService } from './direcciones.service.js';
 
-@Module({})
+@Module({ imports: [DatabaseModule, SecurityModule], controllers: [DireccionesController], providers: [DireccionesRepository, DireccionesService] })
 export class DireccionesModule {}
